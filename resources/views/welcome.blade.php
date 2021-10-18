67219b17de4b13b5ba70a76261c3028b75c20192
Maheen <maheenattagill@gmail.com>
2021-10-18 13:14:19 +0100

    Add user seeder

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+
+
+@section('title', 'Welcome to the sign it visitor book')
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\VisitorController;
+
+Route::get('/', [WelcomeController::class, 'index']);
+Route::resource('/visitors', VisitorController::class);
+
